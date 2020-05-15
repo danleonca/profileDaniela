cfd8696694699074179346a3304ac7498fe29448
danleonca <danleon022688@gmail.com>
2020-05-15 15:50:29 -0500

    changes in box flex

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-let btnNextPage= document.getElementById('projectsPage');
+let btnNextPage = document.getElementById('projectsPage');
 btnNextPage.addEventListener('click', printProjects);
 
-function printProjects(){
-    let printSP= document.getElementById('perfilConteiner');
-    let rootSP= `<div id="titleSP"><h1 id="tileScondPafe">Proyectos JavaScript</h1>
+function printProjects() {
+  let printSP = document.getElementById('perfilConteiner');
+  let rootSP = `<div id="titleSP"><h1 id="tileScondPafe">Proyectos JavaScript</h1>
     <div id="projects">
         <div id"firstProject" class="tarjets">
         <a title="Ver app" href="https://shari12.github.io/CDMX009-Data-Lovers/" target="_blank"><img id="imgFirstProject" class= "imgProject" src="img/pokemon.svg" /></a>
@@ -29,49 +29,49 @@ function printProjects(){
     
     </div>
     `
-printSP.innerHTML = rootSP;
-let btnComent= document.getElementById('btnShare');
-btnComent.addEventListener('click', goToComents);
+  printSP.innerHTML = rootSP;
+  let btnComent = document.getElementById('btnShare');
+  btnComent.addEventListener('click', goToComents);
 
 }
 
-function goToComents(){
-    let tarjetComent= `
+function goToComents() {
+  let tarjetComent = `
     <div id="leaveComents">
     <p><textarea id= "name" cols="40" rows="1" placeholder="Escribe tu nombre"></textarea></p>
     <p><textarea id= "email" cols="40" rows="1" placeholder="Deja tu Email"></textarea></p>
     <p><textarea id= "newComent" cols="40" rows="15" placeholder="Escribe tus comentarios"></textarea></p>
     <p><button class="btnStyle" id="btnSend">Enviar</button></p>
     </div>`
-    let printSP= document.getElementById('perfilConteiner');
-    
-    printSP.innerHTML=tarjetComent;
-    
-    let sendComents= document.getElementById('btnSend');
-    sendComents.addEventListener('click', saveFireBase);
-    }
+  let printSP = document.getElementById('perfilConteiner');
+
+  printSP.innerHTML = tarjetComent;
+
+  let sendComents = document.getElementById('btnSend');
+  sendComents.addEventListener('click', saveFireBase);
+}
 
 
-  
 
-function saveFireBase(){
-    let getName =document.getElementById('name');
-    let getMail= document.getElementById('email');
-    let getComent= document.getElementById('newComent');
-    let date = new Date();
-    date += Date.now();
-    const date1 = date.slice(0, 25);
-    firebase.firestore().collection('allComents').add({
-    
-    name:getName.value,
+
+function saveFireBase() {
+  let getName = document.getElementById('name');
+  let getMail = document.getElementById('email');
+  let getComent = document.getElementById('newComent');
+  let date = new Date();
+  date += Date.now();
+  const date1 = date.slice(0, 25);
+  firebase.firestore().collection('allComents').add({
+
+    name: getName.value,
     mail: getMail.value,
     comments: getComent.value,
     date: date1,
-    
+
   })
     .then((docRef) => {
       console.log('Document written with ID: ', docRef.id);
-     // console.log(coment.value);
+      // console.log(coment.value);
       //const cleanBox = document.getElementById('text-box');
       getName.value = '';
       getMail.value = '';
@@ -82,9 +82,9 @@ function saveFireBase(){
       console.error('Error adding document: ', error);
     });
 }
-function showMe(){
-    let printSP= document.getElementById('perfilConteiner');
-    let myDescription=`<div><div id="quote"><p>"Nada en este mundo debe ser temido... sólo entendido. Ahora es el momento de comprender más, para que podamos temer menos"</p>
+function showMe() {
+  let printSP = document.getElementById('perfilConteiner');
+  let myDescription = `<div class= "allAboutMe"><div id="quote"><p>"Nada en este mundo debe ser temido... sólo entendido. Ahora es el momento de comprender más, para que podamos temer menos"</p>
     <p> Marie Curie </p></div>
    <div id="whoImI"> <h3> ¿Quién soy?</h3>
     <p>Hola! Mi nombre es Daniela soy Front end-developer; me considero una  persona proactiva, valiente y fuerte; con pensamiento crítico, analítico y objetivo. 
@@ -93,10 +93,9 @@ function showMe(){
       </p> </div>
       </div>
     `
-printSP.innerHTML=myDescription
+  printSP.innerHTML = myDescription
 }
 
 
-let btnMe= document.getElementById('aboutMe');
+let btnMe = document.getElementById('aboutMe');
 btnMe.addEventListener('click', showMe);
-   
